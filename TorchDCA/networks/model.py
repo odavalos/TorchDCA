a5061007e0b328f05d2942c4b95c0d8d4572b48f
Oscar Davalos <44180633+odavalos@users.noreply.github.com>
2022-03-01 23:54:02 -0800

    Update model.py

--- TorchDCA/networks/model.py
+++ TorchDCA/networks/model.py
@@ -63,26 +63,26 @@ class DCA(nn.Module):
     
     
     def zinb_loss(x, mean, disp, pi, scale_factor=1.0, ridge_lambda=0.0):
-    eps = 1e-10
-    x = x.type(torch.FloatTensor)
-    sf = scale_factor[:, None]
-    mu = mean.type(torch.FloatTensor) * sf
+        eps = 1e-10
+        x = x.type(torch.FloatTensor)
+        sf = scale_factor[:, None]
+        mu = mean.type(torch.FloatTensor) * sf
 
 
-    # negative binomial
-    t1 = torch.lgamma(disp+eps) + torch.lgamma(x+1.0) - torch.lgamma(x+disp+eps)
-    t2 = (disp+x) * torch.log(1.0 + (mu/(disp+eps))) + (x * (torch.log(disp+eps) - torch.log(mu+eps)))
-    final = t1 + t2
+        # negative binomial
+        t1 = torch.lgamma(disp+eps) + torch.lgamma(x+1.0) - torch.lgamma(x+disp+eps)
+        t2 = (disp+x) * torch.log(1.0 + (mu/(disp+eps))) + (x * (torch.log(disp+eps) - torch.log(mu+eps)))
+        final = t1 + t2
 
-    # ZINB
-    nb_case = final - torch.log(1.0-pi+eps)
-    zero_nb = torch.pow(disp/(disp+mu+eps), disp)
-    zero_case = -torch.log(pi + ((1.0-pi)*zero_nb)+eps)
-    result = torch.where(torch.le(x, 1e-8), zero_case, nb_case)
+        # ZINB
+        nb_case = final - torch.log(1.0-pi+eps)
+        zero_nb = torch.pow(disp/(disp+mu+eps), disp)
+        zero_case = -torch.log(pi + ((1.0-pi)*zero_nb)+eps)
+        result = torch.where(torch.le(x, 1e-8), zero_case, nb_case)
 
-    if ridge_lambda > 0:
-        ridge = ridge_lambda*torch.square(pi)
-        result += ridge
+        if ridge_lambda > 0:
+            ridge = ridge_lambda*torch.square(pi)
+            result += ridge
 
-    result = torch.mean(result)
-    return result
+        result = torch.mean(result)
+        return result
