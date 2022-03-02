2868b19cd2276f39f4f51085944cf10659061441
Oscar Davalos <44180633+odavalos@users.noreply.github.com>
2022-03-01 22:46:17 -0800

    Update model.py

--- TorchDCA/networks/model.py
+++ TorchDCA/networks/model.py
@@ -1,61 +1,60 @@
 import torch
 import torch.nn as nn
-from utils import *
+from loss import *
 
 class DCA(nn.Module):
-    def __init__(self, input_size, latent_dim = 100, sigma=1.):
+    def __init__(self, latent_dim = 50, final_layer = 512, input_size = 13766):
 
         if input_size == None or latent_dim == None:
             raise ValueError('Please provide a value for each input_size, and latent_dim')
           
         super(DCA, self).__init__()
-        self.inp_dim = input_size
-        self.zdim = latent_dim
-        self.sigma = sigma
-
+        self.in_dim = input_size;
+        self.zdim = latent_dim;
+        self.last_layer = final_layer;
+        
         # Autoencoder
-        self.encoder = Encoder(input_size, latent_dim) # encoder
-        self.decoder = Decoder(input_size, latent_dim) # decoder
+        self.encoder = Encoder(input_size = self.in_dim,  latent_dim = self.zdim); # encoder
+        self.decoder = Decoder(input_size = self.in_dim, latent_dim = self.zdim); # decoder
+        
         
         # mean
-        self.decomean = nn.Sequential(
-                                nn.Linear(self.zdim, 512),
-                                MeanAct(),
-            
-                                nn.Linear(512, self.inp_dim),
+        self.mean = nn.Sequential(
+                                nn.Linear(self.last_layer, self.in_dim),
                                 MeanAct()
-                                )
+                                );
         # disp
-        self.decodisp = nn.Sequential(
-                                nn.Linear(self.zdim, 512),
-                                DispAct(),
-
-                                nn.Linear(512, self.inp_dim),
+        self.disp = nn.Sequential(
+                                nn.Linear(self.last_layer, self.in_dim),
                                 DispAct()
-                                )
+                                );
         # pi
-        self.decopi = nn.Sequential(
-                                nn.Linear(self.zdim, 512),
-                                nn.Sigmoid(),
-
-                                nn.Linear(512, self.inp_dim),
+        self.dropout_pi = nn.Sequential(
+                                nn.Linear(self.last_layer, self.in_dim),
                                 nn.Sigmoid()
-                                )
+                                );
+        
+        # reconstruction
+        self.reconstruction = nn.linear(self.last_layer, self.in_dim)
 
 
 
 
     def forward(self, x):
         """
-
         Forward pass of the autoencoder
-
         """
-        z = self.encoder(x)
-        d = self.decoder(z)
-
-        mean = self.decomean(z)
-        disp = self.decodisp(z)
-        pi = self.decopi(z)
+        z = self.encoder(x);
         
-        return z, d, mean, disp, pi
+        d = self.decoder(z);
+
+        mean = self.mean(d);
+
+        disp = self.disp(d);
+
+        pi = self.dropout_pi(d);
+
+        recon = self.reconstruction(d)
+
+
+        return z, mean, disp, pi, recon
