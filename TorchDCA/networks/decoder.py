9488b906b6924832003639ef4763c4557d33798b
Oscar Davalos <44180633+odavalos@users.noreply.github.com>
2022-03-01 22:42:32 -0800

    Update decoder.py

--- TorchDCA/networks/decoder.py
+++ TorchDCA/networks/decoder.py
@@ -1,39 +1,36 @@
 import torch.nn as nn
 
-class Decoder(nn.Module):
-    def __init__(self, latent_dim = 100, input_size = None):
+lass Decoder(nn.Module):
+    def __init__(self, latent_dim = 50, final_layer = 512, input_size = 13766):
+        """
+
+        The Decoder class
+
         """
         
-        The Decoder class
-          
-        """
-        if latent_dim == None or input_size == None:
-            raise ValueError('Must explicitly declare input size and latent space dimension')
-            
-        super(Decoder, self).__init__();
         self.in_dim = input_size;
         self.zdim = latent_dim;
-        
-        # feed forward layers  
-        self.dec = nn.Sequential(
-                                nn.Linear(self.zdim, 512),
+        self.last_layer = final_layer;
+
+        # decoder
+        self.decoder = nn.Sequential(
+                                nn.Linear(self.zdim, 256),
                                 nn.ReLU(),
-                                nn.BatchNorm1d(512),
+                                nn.BatchNorm1d(256),
             
-                                nn.Linear(512, self.inp_dim),
+                                nn.Linear(256, 512),
                                 nn.ReLU(),
-                                nn.BatchNorm1d(self.inp_dim)
-                                           )
+                                nn.BatchNorm1d(512)
+        )
         
-    def forward(self, z):        
-        """
-        
-        Forward pass of the decoder
-        
-        """
+            
+            
+    def forward(self, z):
+            
+            
+        d = self.decoder(z)
 
-        reconst = self.dec(z)   
-        
-        return reconst
 
+
+        return d
 
